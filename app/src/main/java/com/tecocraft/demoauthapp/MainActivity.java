2f31c001183c1004a0bc8168cb996bb72734afff
K Gondaliya <kajalgondaliya1889@gmail.com>
2019-03-22 14:04:26 +0530

    added validation to sdk

--- app/src/main/java/com/tecocraft/demoauthapp/MainActivity.java
+++ app/src/main/java/com/tecocraft/demoauthapp/MainActivity.java
@@ -3,15 +3,12 @@ package com.tecocraft.demoauthapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.tecocraft.authlibrary.Authentication;
-
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -36,29 +33,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String strEmail = email.getText().toString();
             String strPassword = password.getText().toString();
 
-            if (!strEmail.isEmpty() && !strPassword.isEmpty()) {
-                if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
-                    Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_SHORT).show();
-                    email.setText("");
-                } else if (!Pattern.matches("(?=.*?[a-z])(?=.*?[0-9]).{6,}$", strPassword)) {
-                    Toast.makeText(getApplicationContext(), "Password should be atleast 6 character long and contains alphabets and digits", Toast.LENGTH_SHORT).show();
-                    password.setText("");
-                }else if(Authentication.verifyCredential(strEmail,strPassword))
-                {
-                    Toast.makeText(getApplicationContext(), "You are successfully authenticated", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(getApplicationContext(),HomeActivity.class);
+            String error = Authentication.Validate(strEmail, strPassword);
+            if (error.isEmpty()) {
+                if (Authentication.verifyCredential(strEmail, strPassword)) {
+                    Intent i = new Intent(getApplicationContext(), HomeActivity.class);
                     i.putExtra("username",strEmail);
                     startActivity(i);
-                    finish();
-
                 }else {
-                    email.setText("");
-                    password.setText("");
-                    Toast.makeText(getApplicationContext(), "Sorry!! This credential are not valid", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Sorry! this credentials are not valid", Toast.LENGTH_SHORT).show();
                 }
             } else {
-                Toast.makeText(getApplicationContext(), "Please enter email and password", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
             }
+
         }
     }
 }
--- authlibrary/src/main/java/com/tecocraft/authlibrary/Authentication.java
+++ authlibrary/src/main/java/com/tecocraft/authlibrary/Authentication.java
@@ -1,11 +1,21 @@
 package com.tecocraft.authlibrary;
 
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
 /**
  * Created by lenovo on 3/20/2019.
  */
 
 public class Authentication {
 
+    /**
+     * Method to authenticate User
+     * @param email EmailId
+     * @param password Password
+     * @return true value if authentication is successful otherwise return false
+     */
     public static boolean verifyCredential(String email, String password) {
 
         if (!email.isEmpty() && !password.isEmpty()) {
@@ -17,6 +27,28 @@ public class Authentication {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Method to perform Email and password validation
+     * @param email EmailId
+     * @param password Password
+     * @return Error message if validation failed
+     */
+    public static String Validate(String email,String password) {
+        String errorMsg = "";
+        if (!email.isEmpty() && !password.isEmpty()) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                errorMsg = "Please enter valid email";
+            }
+            if (!Pattern.matches("(?=.*?[a-z])(?=.*?[0-9]).{6,}$", password)) {
+                errorMsg = "Password should be minimum 6 character long and contains alphabets and digits";
+            }
+
+        } else {
+            errorMsg = "Please enter email and password";
+        }
+        return errorMsg;
     }
 
 }
